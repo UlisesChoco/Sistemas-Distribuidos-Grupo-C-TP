723cb5999a2724dbd47dc104a19c10e1759a4700
LeanAranda <arandaleandro2003@gmail.com>
2025-09-01 11:01:43 -0300

    SD-27/ definición de servicios para eventos

---
+++ grpc server/src/main/java/com/grupo_c/SistemasDistribuidosTP/ServiceImp/EventServiceImp.java
@@ -0,0 +1,63 @@
+package com.grupo_c.SistemasDistribuidosTP.ServiceImp;
+
+import com.grupo_c.SistemasDistribuidosTP.service.EventServiceClass;
+import com.grupo_c.SistemasDistribuidosTP.service.EventServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventServiceImp extends EventServiceGrpc.EventServiceImplBase {
+
+    @Override
+    public void createEvent (EventServiceClass.EventDto event,
+                             StreamObserver<EventServiceClass.Response> responseStreamObserver){
+
+        /*
+        EventServiceClass.Response response = EventServiceClass.Response.newBuilder().setSucceeded(true).build();
+        responseStreamObserver.onNext(response);
+        responseStreamObserver.onCompleted();
+        */
+    }
+
+    @Override
+    public void modifyEvent (EventServiceClass.EventWithParticipantsDto event,
+                             StreamObserver<EventServiceClass.Response> responseStreamObserver){
+
+    }
+
+    @Override
+    public void deleteEvent (EventServiceClass.EventRequest request,
+                             StreamObserver<EventServiceClass.Response> responseStreamObserver){
+
+    }
+
+    @Override
+    public void assignUserToEvent (EventServiceClass.EventAssignOrDeleteRequest request,
+                                   StreamObserver<EventServiceClass.Response> responseStreamObserver){
+
+    }
+
+    @Override
+    public void deleteUserFromEvent (EventServiceClass.EventAssignOrDeleteRequest request,
+                                     StreamObserver<EventServiceClass.Response> responseStreamObserver){
+
+    }
+
+    @Override
+    public void getEvent (EventServiceClass.EventRequest request,
+                          StreamObserver<EventServiceClass.EventWithParticipantsDto> responseStreamObserver){
+
+    }
+
+    @Override
+    public void getEventsWithParticipantsList (EventServiceClass.Empty request,
+                                               StreamObserver<EventServiceClass.EventsWithParticipantsList> responseStreamObserver){
+
+    }
+
+    @Override
+    public void getEventsWithoutParticipantsList (EventServiceClass.Empty request,
+                                               StreamObserver<EventServiceClass.EventsWithoutParticipantsList> responseStreamObserver){
+
+    }
+}
