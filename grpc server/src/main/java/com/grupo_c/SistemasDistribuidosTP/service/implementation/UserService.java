be57dc7e43c87cbb42be556b4d208e1586fb2956
chocolatada21 <ulisesjustosaucedo@gmail.com>
2025-08-31 23:29:05 -0300

    SD-24 gRPC configuration for UserEntity

---
+++ grpc server/src/main/java/com/grupo_c/SistemasDistribuidosTP/service/implementation/UserService.java
@@ -0,0 +1,52 @@
+package com.grupo_c.SistemasDistribuidosTP.service.implementation;
+
+import com.grupo_c.SistemasDistribuidosTP.service.UserServiceClass;
+import com.grupo_c.SistemasDistribuidosTP.service.UserServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService extends UserServiceGrpc.UserServiceImplBase {
+    @Override
+    public void login(
+            UserServiceClass.LoginRequest request,
+            StreamObserver<UserServiceClass.UserDTO> responseObserver
+    ) {
+
+    }
+    @Override
+    public void createUser(
+            UserServiceClass.OperationsUserDTO request,
+            StreamObserver<UserServiceClass.Response> responseObserver
+    ) {
+
+    }
+    @Override
+    public void modifyUser(
+            UserServiceClass.OperationsUserDTO request,
+            StreamObserver<UserServiceClass.Response> responseObserver
+    ) {
+
+    }
+    @Override
+    public void deleteUser(
+            UserServiceClass.UserRequest request,
+            StreamObserver<UserServiceClass.Response> responseObserver
+    ) {
+
+    }
+    @Override
+    public void getUserList(
+            UserServiceClass.Empty request,
+            StreamObserver<UserServiceClass.UserListResponse> responseObserver
+    ) {
+
+    }
+    @Override
+    public void getUser(
+            UserServiceClass.UserRequest request,
+            StreamObserver<UserServiceClass.UserDTO> responseObserver
+    ) {
+
+    }
+}
