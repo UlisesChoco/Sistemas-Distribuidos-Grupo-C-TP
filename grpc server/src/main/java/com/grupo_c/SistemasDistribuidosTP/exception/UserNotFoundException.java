3194e8a0a9728c16c5f88cb117c570d68f11f205
chocolatada21 <ulisesjustosaucedo@gmail.com>
2025-09-14 00:51:38 -0300

    SD-24/Add UserNotFoundException

---
+++ grpc server/src/main/java/com/grupo_c/SistemasDistribuidosTP/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.grupo_c.SistemasDistribuidosTP.exception;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
