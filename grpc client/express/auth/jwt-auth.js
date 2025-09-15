d62d3c08437841f9bba8c9b05201261a6f5fe262
chocolatada21 <ulisesjustosaucedo@gmail.com>
2025-09-15 13:57:21 -0300

    SD-38/User views and JWT verification on certain routes

--- grpc client/express/auth/jwt-auth.js
+++ grpc client/express/auth/jwt-auth.js
@@ -3,11 +3,11 @@ const jwt = require("jsonwebtoken"); //para validar token entrante
 const authenticateToken = (req, res, next) => {
   const token = req.cookies.token;
 
-  if (token == null) return res.render("index", {});
+  if (token == null) return res.render("index", {}); //si no hay token lo mandamos al login
 
   jwt.verify(token, process.env.JWT_PRIVATE_KEY, (err, payload) => {
     if (err) {
-        return res.render("index", {});
+        return res.render("index", {});//si el token no es valido lo mandamos al login
     };
     req.user = {username: payload.sub, roles: payload.roles};
     next();
