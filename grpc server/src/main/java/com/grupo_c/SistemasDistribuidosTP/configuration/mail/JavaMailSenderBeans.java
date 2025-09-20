2068eb954e4eeba17adbdf80942a3f72a3469cc3
chocolatada21 <ulisesjustosaucedo@gmail.com>
2025-09-19 22:40:30 -0300

    SD-24/Match JavaMailSender host and port to Docker

--- grpc server/src/main/java/com/grupo_c/SistemasDistribuidosTP/configuration/mail/JavaMailSenderBeans.java
+++ grpc server/src/main/java/com/grupo_c/SistemasDistribuidosTP/configuration/mail/JavaMailSenderBeans.java
@@ -9,8 +9,8 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 @Configuration
 public class JavaMailSenderBeans {
     //SMTP_SERVER_HOST y SMTP_SERVER_PORT son valores temporales por ahora. hay que cambiarlos cuando dockerizemos el proyecto
-    private final static String SMTP_SERVER_HOST = "localhost";
-    private final static int SMTP_SERVER_PORT = 32768;
+    private final static String SMTP_SERVER_HOST = "mailhog";
+    private final static int SMTP_SERVER_PORT = 1025;
     private final static String EMAIL = "gestion_usuarios@ongempujecomunitario.com";
     private final static String EMAIL_SUBJECT = "Alta de usuario";
     @Bean
