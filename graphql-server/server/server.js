41e95bf1e6494b5e1c840225ea3ec3082986f5ad
LeanAranda <arandaleandro2003@gmail.com>
2025-10-19 18:10:50 -0300

    SD-68/Habilited CORS for GraphQl endpoints

--- graphql-server/server/server.js
+++ graphql-server/server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('../schema/mergeSchemas');
 
 const app = express();
 const port = 4000;
+
+//cors habilitado para todas las rutas
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema,
