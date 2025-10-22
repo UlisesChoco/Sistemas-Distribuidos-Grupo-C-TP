7f22684efe6842a1ba30891d3430617721a4ab71
chocolatada21 <ulisesjustosaucedo@gmail.com>
2025-10-22 10:15:13 -0300

    SD-71/Donation entity, repository and service interface

---
+++ grpc server/src/main/java/com/grupo_c/SistemasDistribuidosTP/entity/Donation.java
@@ -0,0 +1,32 @@
+package com.grupo_c.SistemasDistribuidosTP.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "donations")
+@AllArgsConstructor
+@Getter @Setter
+@ToString
+public class Donation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Inventory.Category category;
+
+    @Column(length = 200)
+    private String description;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private Boolean isDeleted;
+
+    public Donation() {
+        this.quantity = 0;
+    }
+}
---
+++ grpc server/src/main/java/com/grupo_c/SistemasDistribuidosTP/repository/IDonationRepository.java
@@ -0,0 +1,12 @@
+package com.grupo_c.SistemasDistribuidosTP.repository;
+
+import com.grupo_c.SistemasDistribuidosTP.entity.Donation;
+import com.grupo_c.SistemasDistribuidosTP.entity.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IDonationRepository extends JpaRepository<Donation, Long> {
+    boolean existsByCategoryAndDescription(Inventory.Category category, String description);
+    Donation findByCategoryAndDescription(Inventory.Category category, String description);
+}
